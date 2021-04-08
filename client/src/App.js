4a6148debdaa791c92367e778ab691d89cfb60c1
Christie Corcoran <christiecorcoran30@gmail.com>
2021-04-07 21:10:44 -0400

    Get React client and Express server talking.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [apiResponse, setResponse] = useState("");
+
+  function callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then( res => res.text() )
+      .then( res => setResponse( res ))
+      .catch(err => setResponse(err));
+  };
+
+  callAPI();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          API Response: {apiResponse}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       </header>
     </div>
   );
 }
