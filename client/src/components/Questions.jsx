b7d32953c13788d48891db86f4cc9bb41681955f
Christie Corcoran <christiecorcoran30@gmail.com>
2021-04-12 16:32:22 -0400

    Question page tweaks; added question details

--- client/src/components/Questions.jsx
+++ client/src/components/Questions.jsx
@@ -4,9 +4,9 @@ import '../css/journal.css';
 import '../sass/appsass.scss';
 
 var questions = [
-    {"id": 1, "question": "Will Vitamin D save my life?", "user": "vitahack", "vote": 0, "category": 4},
-    {"id": 2, "question": "Could my cell phone kill me?", "user": "paranoid", "vote": 0, "category": 1},
-    {"id": 3, "question": "Is it okay to cleanse your body by fasting from time to time?", "vote": 0, "category": 2}
+    {"id": 1, "question": "Will Vitamin D save my life?", "details": "My doctors has me taking four times the recommended dose.  Can this really be healthy?", "user": "vitahack", "vote": 0, "category": 4},
+    {"id": 2, "question": "Could my cell phone kill me?", "details": "It seems like this could be an issue.  Curious if anyone knows of some research around this topic.", "user": "paranoid", "vote": 0, "category": 1},
+    {"id": 3, "question": "Is it okay to cleanse your body by fasting from time to time?", "details": "My next door neighbor keeps telling me that I should cleanse my body to remove toxins.  Is this really a thing?", "vote": 0, "category": 2}
 ];
 
 /*------------- Save for later ------------------
@@ -34,7 +34,8 @@ const Question = props => {
             0<br/> Answers
         </div>
         <div className="question">
-            <Link className="strong" to="#">{props.question}</Link>
+            <Link className="strong" to="#">{props.question}</Link><br/>
+            <span>{props.details}</span>
         </div>
         <br />
         <hr/>
@@ -47,12 +48,12 @@ function Questions() {
     
     return (
         <div id="questions" className="container">
-            <h3 className="mb-4">All Questions</h3>
+            <h3 className="mb-4">All Questions<Link id="btnAsk" className="btn btn-primary" role="button" to="#">Ask Question</Link></h3>
             <hr />
             {questions.map((item, i) => {
                 return (
                     <div>
-                        <Question question={item.question} user={item.user} vote={item.vote} />
+                        <Question question={item.question} details={item.details} user={item.user} vote={item.vote} />
                     </div>
                 )
             })}
